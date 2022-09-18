47534ba1e1d4e68b18e95faa784b9db5ffccb25f
Michelle Quach <61102352+mtquach2@users.noreply.github.com>
2022-09-18 20:57:42 +0000

    Added P5.play

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
+var cards = [];
+var index = 0;
+let values = ['A', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+	'J', 'Q', 'K'];
+let suits = ['diamonds', 'hearts', 'spades', 'clubs'];
+
 function getWindow() {
-  let w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight || e.clientHeight || g.clientHeight;
-  return { w: x, h: y }
+	let w = window,
+		d = document,
+		e = d.documentElement,
+		g = d.getElementsByTagName('body')[0],
+		x = w.innerWidth || e.clientWidth || g.clientWidth,
+		y = w.innerHeight || e.clientHeight || g.clientHeight;
+	return { w: x, h: y };
 }
 
 function randColor() {
-  return color(random(255), random(255), random(255));
+	return color(random(255), random(255), random(255));
 }
 
-let img;
-let cards = [];
-let values = ['A', '02', '03', '04', '05', '06', '07', '08', '09', '10',
-    'J', 'Q', 'K'];
-let suits = ['diamonds', 'hearts', 'spades', 'clubs'];
-
 function preload() {
-  img = loadImage("cards/card_back.png");
-
-  for (let s in suits) {
-    for (let v in values) {
-      cards.push(loadImage('cards/card_' + suits[s] + '_' + values[v] + '.png'));
-    }
-  }
+	for (const suit of suits) {
+		for (const value of values) {
+			cards.push(loadImage(`cards/card_${suit}_${value}.png`));
+		}
+	}
 }
 
 function setup() {
-  let win = getWindow();
-  createCanvas(win.w, win.h);
-  for (let x = 0; x < 52; x++) {
-      image(cards[x], x*10, 0, 64, 64);
-  }
+	let win = getWindow();
+	createCanvas(win.w, win.h);
 }
 
 function draw() {
-  
+	staticRender();
+}
+
+function staticRender() {
+	background(0, 0, 0);
+
+	if (index < 52) {
+		if (mouse.pressed()) {
+			sprite = new Sprite(mouseX, mouseY, 64, 64);
+			sprite.addImage('face', cards[index]);
+			index++;
+		}
+	}
+	else {
+		index = 0;
+	}
 }
