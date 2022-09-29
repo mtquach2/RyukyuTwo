2781e5ed22ee8154d463628ab05d88d872ca77e2
Jon Rahoi <jon@rahoi.com>
2022-09-28 23:21:54 -0700

    weirdness is avoided

--- src/main.ts
+++ src/main.ts
@@ -38,4 +38,43 @@ const _app = new p5(p5Instance => {
   };
 }, document.getElementById('app')!);
 
+
+
+/*
+// THIS IS AN EXAMPLE OF P5.PLAY IN INSTANCE MODE
+// BECAUSE WE'RE INJECTING P5 && P5.PLAY DIRECTLY INTO THE BROWSER,
+// OUR P5 HERE HAS P5.PLAY ALREADY IN IT
+
+new p5(p => {
+  let bg, sprite;
+
+  p.setup = function () {
+    p.createCanvas(800, 600).mousePressed(reset);
+    bg = p.color(0o350);
+    sprite = p.createSprite(p.width>>1, p.height>>1, 0o100, 0o100);
+    reset();
+  };
+
+  p.draw = function () {
+    p.background(bg);
+    bounce();
+    p.drawSprites();
+  };
+
+  function reset() {
+    sprite.setVelocity(p.random(-5, 5), p.random(-5, 5));
+    sprite.shapeColor = p.color('#' + p.hex(~~p.random(0x1000), 3));
+  }
+
+  function bounce() {
+    const { position: { x, y }, width: w, height: h } = sprite,
+          ww = w >> 1, hh = h >> 1;
+
+    if (x < ww || x + ww >= p.width)   sprite.velocity.x *= -1;
+    if (y < hh || y + hh >= p.height)  sprite.velocity.y *= -1;
+  }
+});
+*/
+
 _app;
+
--- src/script.js
+++ src/script.js
@@ -1,32 +1,41 @@
-// import { Cards } from './modules/Cards.js';
-console.log("AFTER IMPORT")
-// let deck = new Cards();
+/**
+ * p5.Play Instance Mode Example (v1.0.1)
+ * GoToLoop (2019-Apr-04)
+ *
+ * https://GitHub.com/molleindustria/p5.play/issues/12
+ * https://Bl.ocks.org/GoSubRoutine/cf739128f389766f5d9cb631c9c358a6
+ */
 
-function getWindow() {
-  let w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight || e.clientHeight || g.clientHeight;
-  return { w: x, h: y };
-}
+ "use strict";
 
-function randColor() {
-  return color(random(255), random(255), random(255));
-}
-
-function preload() {
-  // deck.load();
-}
-
-function setup() {
-  let win = getWindow();
-  console.log(`window is ${win}`)
-  createCanvas(win.w, win.h);
-  background(randColor())
-}
-
-function draw() {
-  // deck.staticRender();
-}
+ new p5(p => {
+   let bg, sprite;
+ 
+   p.setup = function () {
+    console.log(p)
+     p.createCanvas(800, 600)
+    //  p.mousePressed(reset);
+     bg = p.color(0o350);
+     sprite = p.createSprite(p.width>>1, p.height>>1, 0o100, 0o100);
+     reset();
+   };
+ 
+   p.draw = function () {
+     p.background(bg);
+     bounce();
+     p.drawSprites();
+   };
+ 
+   function reset() {
+     sprite.setVelocity(p.random(-5, 5), p.random(-5, 5));
+     sprite.shapeColor = p.color('#' + p.hex(~~p.random(0x1000), 3));
+   }
+ 
+   function bounce() {
+     const { position: { x, y }, width: w, height: h } = sprite,
+           ww = w >> 1, hh = h >> 1;
+ 
+     if (x < ww || x + ww >= p.width)   sprite.velocity.x *= -1;
+     if (y < hh || y + hh >= p.height)  sprite.velocity.y *= -1;
+   }
+ });
