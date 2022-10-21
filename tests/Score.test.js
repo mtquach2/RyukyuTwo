f5ecdc22281f7c3d1875b87c5b58bce884f5ee3b
mtquach <mtquach2@dons.usfca.edu>
2022-10-20 19:02:07 -0700

    made score test more readable

--- tests/Score.test.js
+++ tests/Score.test.js
@@ -10,61 +10,61 @@ test('Check updateScore for 5K', () => {
     expect(s.pointsMap.has('5K')).toEqual(true);
 });
 
-test('Check for updateScore for RSF', () => {
+test('Check updateScore for RSF', () => {
     s.updateScore('RSF');
     expect(s.currentScore).toBe(2800);
     expect(s.pointsMap.has('RSF')).toEqual(true);
 });
 
-test('Check for updateScore for SF', () => {
+test('Check updateScore for SF', () => {
     s.updateScore('SF');
     expect(s.currentScore).toBe(2400);
     expect(s.pointsMap.has('SF')).toEqual(true);
 });
 
-test('Check for updateScore for 4K', () => {
+test('Check updateScore for 4K', () => {
     s.updateScore('4K');
     expect(s.currentScore).toBe(2000);
     expect(s.pointsMap.has('4K')).toEqual(true);
 });
 
-test('Check for updateScore for FH', () => {
+test('Check updateScore for FH', () => {
     s.updateScore('FH');
     expect(s.currentScore).toBe(1800);
     expect(s.pointsMap.has('FH')).toEqual(true);
 });
 
-test('Check for updateScore for ST', () => {
+test('Check updateScore for ST', () => {
     s.updateScore('ST');
     expect(s.currentScore).toBe(1400);
     expect(s.pointsMap.has('ST')).toEqual(true);
 });
 
-test('Check for updateScore for FL', () => {
+test('Check updateScore for FL', () => {
     s.updateScore('FL');
     expect(s.currentScore).toBe(1000);
     expect(s.pointsMap.has('FL')).toEqual(true);
 });
 
-test('Check for updateScore for 3K', () => {
+test('Check updateScore for 3K', () => {
     s.updateScore('3K');
     expect(s.currentScore).toBe(800);
     expect(s.pointsMap.has('3K')).toEqual(true);
 });
 
-test('Check for updateScore for 2P', () => {
+test('Check updateScore for 2P', () => {
     s.updateScore('2P');
     expect(s.currentScore).toBe(600);
     expect(s.pointsMap.has('2P')).toEqual(true);
 });
 
-test('Check for updateScore for 1P', () => {
+test('Check updateScore for 1P', () => {
     s.updateScore('1P');
     expect(s.currentScore).toBe(200);
     expect(s.pointsMap.has('1P')).toEqual(true);
 });
 
-test('Check for updateScore for H', () => {
+test('Check updateScore for H', () => {
     s.updateScore('H');
     expect(s.currentScore).toBe(0);
     expect(s.pointsMap.has('H')).toEqual(true);
