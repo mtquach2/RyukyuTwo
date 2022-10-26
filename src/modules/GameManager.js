6393ba014be721314036a9240ce16ec7d441f3a2
mtquach <mtquach2@dons.usfca.edu>
2022-10-26 15:40:25 -0700

    Changed game over to send back to main menu screen

--- src/modules/GameManager.js
+++ src/modules/GameManager.js
@@ -76,11 +76,13 @@ function menu(width, height) {
     p.fill(0, 0, 0);
     p.textSize(64);
     p.text("CLICK TO PLAY GAME", width / 3, height / 3, 400, 150);
+}
 
-    if (p.mouseIsPressed) {
-        if (width / 3 < p.mouseX && p.mouseX < width / 3 + 400 && height / 3 < p.mouseY && p.mouseY < height / 3 + 150) {
+function menuState(width, height, x, y) {
+    if (game.getState() == 0) {
+        if (width / 3 < x && x < width / 3 + 400 && height / 3 < y && y < height / 3 + 150) {
             p.textSize(20);
-            game.setState(1);
+            resetGame(1);
         }
     }
 }
@@ -113,14 +115,7 @@ function gameOver(width, height) {
     p.stroke(0, 0, 0);
     p.fill(0, 0, 0);
     p.textSize(64);
-    p.text("GAME OVER, CLICK TO RESET", width / 3, height / 3, 400, 400);
-
-    if (p.mouseIsPressed) {
-        if (width / 3 < p.mouseX && p.mouseX < width / 3 + 400 && height / 3 < p.mouseY && p.mouseY < height / 3 + 400) {
-            p.textSize(20);
-            resetGame(4);
-        }
-    }
+    p.text("GAME OVER, CLICK TO MENU", width / 3, height / 3, 400, 400);
 }
 
 function cont(width, height) {
@@ -149,6 +144,15 @@ function contStates(width, height, x, y) {
         if ((width / 3 - width / 25) < x && x < (width / 3 - width / 25) + 150 && height / 2 < y && y < height / 2 + 100) {
             // If YES button is clicked, omikuji
             game.setState(3);
+        }
+    }
+}
+
+function gameOverState(width, height, x, y) {
+    if (game.getState() == 4) {
+        if (width / 3 < x && x < width / 3 + 400 && height / 3 < y && y < height / 3 + 400) {
+            p.textSize(20);
+            game.setState(0);
         }
     }
 }
@@ -205,6 +209,8 @@ GM.mouseClicked = function (x, y) {
     game.updateTopDisplay(x, y);
     board.chooseCol(y, game.recentMoves, score);
     contStates(p.windowWidth, p.windowHeight, x, y);
+    gameOverState(p.windowWidth, p.windowHeight, x, y);
+    menuState(p.windowWidth, p.windowHeight, x, y);
 }
 
 export { game, score, timer, board, GM }; //exporting for tests and one instance throughout code
