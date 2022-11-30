1274d39b42fa7b3ed69f5fdb3205d16be9162b52
mtquach2 <61102352+mtquach2@users.noreply.github.com>
2022-11-30 11:09:04 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
     build: {
         emptyOutDir: true
-    },
-    plugins: [
-        viteStaticCopy({
-        targets: [
-            {
-            src: './static/*',
-            dest: './static'
-            }
-        ]
-        })
-    ]
+    }
 });
