5281083038ed730e624a5e58a1b2b5ede7bc2669
mtquach2 <61102352+mtquach2@users.noreply.github.com>
2022-11-30 00:07:31 -0800

    Update LeaderboardInput.js

--- src/modules/LeaderboardInput.js
+++ src/modules/LeaderboardInput.js
@@ -179,7 +179,7 @@ export class LeaderboardInput {
         return this.selectorKeypress(level);
     }
 
-    leaderboardState(x, y, width, height, scaleX, scaleY) {
+    leaderboardState(x, y, width, height, scaleX, scaleY, level) {
         // Left Button
         if ((width * 0.71) < x && x < (width * 0.71 + 60 * scaleX) && y > (height * 0.7) && y < (height * 0.7 + 60 * scaleY)) {
             this.soundManager.playLeaderboardClick();
